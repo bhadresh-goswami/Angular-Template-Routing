8ff7936f53a5ea7ed202da05d4388f44292cfdfc
bhadresh gosai <45452903+bhadresh-goswami@users.noreply.github.com>
2020-07-29 17:50:38 -0400

    upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { HomeComponent } from './common/home/home.component';
+import { AboutusComponent } from './common/aboutus/aboutus.component';
+import { ContactusComponent } from './common/contactus/contactus.component';
+import { NotfoundComponent } from './common/notfound/notfound.component';
+import { SigninComponent } from './user/signin/signin.component';
+import { SignupComponent } from './user/signup/signup.component';
+import { EditprofileComponent } from './user/editprofile/editprofile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home', component:HomeComponent
+  },
+  {
+    path:'about-us', component:AboutusComponent
+  },
+  {
+    path:'contact-us', component:ContactusComponent
+  },
+  {
+    path:'signup', component:SignupComponent
+  },
+  {
+    path:'signin', component:SigninComponent
+  },
+  {
+    path:'edit', component:EditprofileComponent
+  },
+  {
+    path:'', redirectTo:'/home', pathMatch:'full'
+  },
+  {
+    path:'**',component:NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';//this is the file that holds the routes
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './design/header/header.component';
+import { FooterComponent } from './design/footer/footer.component';
+import { NavigationComponent } from './design/navigation/navigation.component';
+import { HomeComponent } from './common/home/home.component';
+import { AboutusComponent } from './common/aboutus/aboutus.component';
+import { ContactusComponent } from './common/contactus/contactus.component';
+import { NotfoundComponent } from './common/notfound/notfound.component';
+import { SigninComponent } from './user/signin/signin.component';
+import { SignupComponent } from './user/signup/signup.component';
+import { EditprofileComponent } from './user/editprofile/editprofile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent,
+    NotfoundComponent,
+    SigninComponent,
+    SignupComponent,
+    EditprofileComponent
   ],
   imports: [
     BrowserModule,
